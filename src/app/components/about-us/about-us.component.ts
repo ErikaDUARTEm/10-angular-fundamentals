972299e3bd231f034273b07a20b133c8511c55bc
erika.duarte <erika.duarte.mora@gmail.com>
2025-01-21 10:56:33 -0500

    feat: svg header

--- src/app/components/about-us/about-us.component.ts
+++ src/app/components/about-us/about-us.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-about-us',
   imports: [],
   templateUrl: './about-us.component.html',
-  styleUrl: './about-us.component.scss'
+  styleUrl: './about-us.component.scss',
 })
 export class AboutUsComponent {
-
+  headItems = signal([
+    {
+      number: '01',
+      title: 'Our Mission',
+      description: 'Make humans better—a lot better',
+      hasLine: true,
+      id: 1,
+    },
+    {
+      number: '02',
+      title: 'Our Vision',
+      description: 'Lead the future of Invisible Computing',
+      hasLine: true,
+      id: 2,
+    },
+    {
+      number: '03',
+      title: 'Our Ambition',
+      description: 'Simplify Heads-Up Computing',
+      hasLine: false,
+      id: 3,
+    },
+  ]);
 }
